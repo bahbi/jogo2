9ed5b528c9f8719c63283192c681491d0d556519
Bárbara Pires <65234688+bahbi@users.noreply.github.com>
2021-04-30 11:07:00 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+function setup() {
+  createCanvas(500, 400);
+}
+
+function draw() {
+  background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialDoCarro();
+  verificacolisao();
+  incluiPontos()
+  marcaPonto()
+}
+
+
+
+
+
+
+
